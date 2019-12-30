2be2c59bf5633f6293e31c89d693e6eb6d7102f9
Marina8989 <marina.mill209@gmail.com>
2019-12-29 18:50:23 -0800

    solved couple more algorithms

--- main.js
+++ main.js
@@ -4,4 +4,19 @@ function addition(a, b) {
     return a + b;
 }
 
-addition(2, 3);
+//addition(2, 3);
+
+//2. Write a function that takes an integer minutes and converts it to seconds.
+function convert(minutes) {
+    return minutes * 60;
+}
+//convert(5);
+
+//3. Write a function that takes the base and height of a triangle and return its area.
+//The are of a triangle is: (base * height) / 2;
+function triArea(base, height) {
+    return (base * height) / 2;
+}
+//triArea(4, 5);
+
+//4.
