12ae044167640aed0ac4a708e9be89f1854f15d6
Marina8989 <marina.mill209@gmail.com>
2019-12-29 18:42:52 -0800

    solved first algorithm

---
+++ main.js
@@ -0,0 +1,7 @@
+//1. Return the Sum of Two Numbers
+// Create a function that takes two numbers as arguments and return their sum.
+function addition(a, b) {
+    return a + b;
+}
+
+addition(2, 3);
