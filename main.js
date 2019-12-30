d6d34419ae3200f9b5703d052cd626dbf79d3385
Marina8989 <marina.mill209@gmail.com>
2019-12-29 19:26:16 -0800

    solved one more algorithm

--- main.js
+++ main.js
@@ -19,4 +19,9 @@ function triArea(base, height) {
 }
 //triArea(4, 5);
 
-//4.
+//4.Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+function addition(num) {
+    let z = 1
+    return num = num + z;
+}
+// addition(2);
