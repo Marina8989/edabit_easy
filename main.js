ddf793ca7dd9ce9354bd12dd2b8260b398a4100c
Marina8989 <marina.mill209@gmail.com>
2019-12-31 22:06:52 -0800

    solved one more

--- main.js
+++ main.js
@@ -211,3 +211,9 @@ function hasSpaces(str) {
 function numArgs() {
     return arguments.length;
 }
+//31. Create a function that takes a base number and an exponent number and returns the calculation.
+function calculateExponent(num, exp) {
+    return Math.pow(num, exp);
+}
+
+//32.
