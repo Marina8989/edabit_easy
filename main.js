87c627593b2348e3af4d71032a8da1881fa2d476
Marina8989 <marina.mill209@gmail.com>
2020-01-06 13:13:06 -0800

    test

--- main.js
+++ main.js
@@ -211,7 +211,7 @@ function hasSpaces(str) {
 function numArgs() {
     return arguments.length;
 }
-//31. Create a function that takes a base number and an exponent number and returns the calculation.
+//31.  Create a function that takes a base number and an exponent number and returns the calculation.
 function calculateExponent(num, exp) {
     return Math.pow(num, exp);
 }
