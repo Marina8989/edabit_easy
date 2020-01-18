3db08a61ea0790384bd5473f7881d99a263e344e
Marina8989 <marina.mill209@gmail.com>
2020-01-17 20:47:52 -0800

    solved one more algrm

--- main.js
+++ main.js
@@ -216,4 +216,28 @@ function calculateExponent(num, exp) {
     return Math.pow(num, exp);
 }
 
-//32.
+//32.Per 6 coffee cups I buy, I get a 7th cup free. In total, I get 7 cups. Create a function that takes n cups bought and return as an integer the total number of cups I would get.
+
+// Examples
+
+// totalCups(6) ➞ 7
+
+// totalCups(12) ➞ 14
+
+//totalCups(213) ➞ 248
+function totalCups(n) {
+    return n + Math.floor(n / 6);
+}
+
+//32. Create a function that takes a string as an argument. The string is a random name.
+
+// If the last character of the name is an "n", return true.
+// If the last character of the name is not an "n", return false.
+function isLastCharacterN(word) {
+    for (i = 0; i < word.length; i++) {
+        if (word[word.length - 1] === "n") {
+            return true;
+        }
+        return false;
+    }
+}
